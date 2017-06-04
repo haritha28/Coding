c4a9a26e58c9ce4c2a3ad1e81ee96a4dc064304d
Haritha28 <haritha.h20@gmail.com>
2017-06-04 21:39:47 +0530

    Ifinite loop no output

--- Tower_of_Hanoi/toh.cpp
+++ Tower_of_Hanoi/toh.cpp
@@ -1,40 +1,54 @@
 /*Implementing the tower of Hanoi using the stack as array*/
 #include<iostream>
+#include<climits>
 #define SIZE 7
 using namespace std;
 
-int main () {
-  //declare the source, destination and the auxillary
-  int src[SIZE+1], dest[SIZE+1], aux[SIZE+1];
-  int srcTop, destTop, auxTop;
-  //set last values to MAX
-  dest[SIZE] = aux[SIZE] = INT_MAX;
-  int ind;
-  //intialise the source with 1 - 7 values
-  for(ind = 0; ind < SIZE; ind++) {
-    src[ind] = ind + 1;
-  }
-  //intialise the dst and aux to -1
-  for(ind = 0; ind < SIZE; ind++){
-    aux[ind] = dest[ind] =  -1;
-  }
+//declare the stack as array
+int src[SIZE], dest[SIZE], aux[SIZE];
+int srcTop, destTop, auxTop;
 
-  while(1) {
-    //S <-> D movement
-    if(src[srcTop] < dest[destTop]) {
+void print () {
+  for(int ctr = 0; ctr < SIZE; ctr++)
+    cout<<src[ctr];
+  cout<<endl;
+  for(int ctr = 0; ctr < SIZE; ctr++)
+    cout<<dest[ctr];
+  cout<<endl;
+  for(int ctr = 0; ctr < SIZE; ctr++)
+      cout<<aux[ctr];
+  cout<<endl;
+}
+
+int main () {
+
+int ind;
+
+for(ind = 0; ind < SIZE; ind++)
+  src[ind] = ind + 1;
+for(ind = 0; ind < SIZE; ind++)
+  aux[ind] = dest[ind] = -1;
+aux[SIZE] = dest[SIZE] = INT_MAX;
+//print();
+
+srcTop = 0;
+destTop = auxTop = SIZE;
+
+while(1){
+    if(src[srcTop] < dest[destTop]){
       destTop--;
       dest[destTop] = src[srcTop];
       src[srcTop] = -1;
       srcTop++;
     } else {
       srcTop--;
-      src[srcTop]= dest[destTop];
-      dest[destTop];
+      src[srcTop] = dest[destTop];
+      dest[destTop] = -1;
       destTop++;
     }
     if(destTop == 0)
-      break;
-    //S<->A movement
+        break;
+
     if(src[srcTop] < aux[auxTop]) {
       auxTop--;
       aux[auxTop] = src[srcTop];
@@ -42,31 +56,24 @@ int main () {
       srcTop++;
     } else {
       srcTop--;
-      src[srcTop]= aux[auxTop];
-      aux[auxTop]= -1;
-      auxTop++;
-    }
-    //D<->A movement
-    if(aux[auxTop] < dest[destTop]){
-      destTop--;
-      dest[destTop] = aux[auxTop];
+      src[srcTop] = aux[auxTop];
       aux[auxTop] = -1;
-      auxTop++;
-    } else {
+      srcTop++;
+    }
+
+    if(dest[destTop] < aux[auxTop]) {
       auxTop--;
       aux[auxTop] = dest[destTop];
       dest[destTop] = -1;
       destTop++;
+    } else {
+      destTop--;
+      dest[destTop] = aux[auxTop];
+      aux[auxTop] = -1;
+      auxTop++;
     }
-    if(desTop == 0 )
-      break;
-
-
-  }
-
-
-
-
-
-
+    if(destTop == 0)
+        break;
+}
+return 0;
 }
