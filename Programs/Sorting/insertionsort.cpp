d5ecc29949775a1965c8729c43bd1cd3d37d12d5
Haritha28 <haritha.h20@gmail.com>
2017-06-07 07:23:21 +0530

    Insertion sorting using the for loop

--- Programs/Sorting/insertionsort.cpp
+++ Programs/Sorting/insertionsort.cpp
@@ -1,31 +1,32 @@
 #include<iostream>
 using namespace std;
 
-void insertion_sort(int A[], int n) {
+int arr[] = {9,6,5,0,3,2,7,1};
+int size = sizeof(arr)/sizeof(arr[0]);
 
-  int j, key;
+void print() {
+  for(int ind = 0; ind < size; ind++)
+    cout<<arr[ind]<<" ";
+}
 
-  for(int j = 1; j < n; j++) {
-    key = A[j];
-    i = j - 1;
-    while(i >= 0 && A[i]>key) {
-      A[i+1] = A[i];
-      i = i - 1;
+void insertionsort() {
+    int key, move, pos;
+
+
+    for(int index = 0; index < size ; index++) { //traversal array
+      key = arr[index];
+    for( pos = 0; pos < key; pos++)//loop to find the correct point to be inserted
+          if(arr[index] > key)
+            break;
+
+    for(move = index; move > pos; move--)
+        arr[move] = arr[move-1];
+    arr[pos] = key;
+
     }
-    A[i+1]= key;
-  }
-
-  for(int ind = 0; ind < n; ind++){
-    cout<<A[ind]<<" ";
-  }
-
 }
 
 int main () {
-
-  int size;
-  int arr[] = {9,6,5,0,3,2,7,1};
-  size = sizeof(arr)/sizeof(arr[0]);
-  insertion_sort(arr, size);
-
+  insertionsort();
+  print();
 }
